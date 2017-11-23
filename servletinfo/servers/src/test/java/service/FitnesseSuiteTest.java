aca45d6c26868b258f83f0ba9f6d077e7e57ec5e
sridhar <sridhar.yathiraju@gmail.com>
2017-11-23 12:39:19 +0530

    servers

---
+++ servletinfo/servers/src/test/java/service/FitnesseSuiteTest.java
@@ -0,0 +1,10 @@
+package service;
+
+import org.junit.runner.RunWith;
+
+import fitnesse.junit.FitNesseRunner;
+
+@RunWith(FitnesseRunner.class)
+@FitNesseRunner.Suite("FitNesse.SuiteAcceptanceTests")
+public class FitnesseSuiteTest {
+}
